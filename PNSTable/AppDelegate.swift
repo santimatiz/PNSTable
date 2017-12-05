1e8422d5ebfa763cda66816d26cc1fa5d5dc2924
santimatiz <smatiz@hotmail.com>
2017-12-05 10:44:28 -0500

    Initial Commit

---
+++ PNSTable/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PNSTable
+//
+//  Created by Santiago Matiz on 5/12/17.
+//  Copyright © 2017 Intelyclick. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PNSTable/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PNSTable
+//
+//  Created by Santiago Matiz on 5/12/17.
+//  Copyright © 2017 Intelyclick. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
