d3738fe6d89a2e91157bcd7047b42c6f80c209d2
santimatiz <smatiz@hotmail.com>
2017-12-08 21:16:33 -0500

    Change code
    Change code

--- PNSTable/ViewController.swift
+++ PNSTable/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: NSViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
     override var representedObject: Any? {
@@ -21,7 +23,55 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+}
 
+extension ViewController: NSTableViewDataSource {
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return 3;
+    }
+}
 
+extension ViewController: NSTableViewDelegate {
+    
+    fileprivate enum CellIdentifiers {
+        static let NameCell = "NameCellID"
+        static let DateCell = "DateCellID"
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+       
+        var text: String = ""
+        var cellIdentifier: String = ""
+        
+        
+        NSLog( " Identifier: \(tableColumn) row: \(row) ");
+        
+        // 2
+        if tableColumn == tableView.tableColumns[0] {
+            text = "Uno \(row)"
+            cellIdentifier = CellIdentifiers.NameCell
+        } else if tableColumn == tableView.tableColumns[1] {
+            text = "dos \(row)"
+            cellIdentifier = CellIdentifiers.DateCell
+        } else {
+        text = "paso";
+        }
+        
+        // 1
+        var cellView: NSTableCellView = tableView.makeView(withIdentifier: tableColumn!.identifier, owner: self) as! NSTableCellView
+
+      
+  
+        cellView.textField?.stringValue = text;
+        /*
+       if let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: cellIdentifier), owner: nil) as? NSTableCellView {
+            cell.textField?.stringValue = text
+             NSLog( " Retorna Celda ");
+            return cell;
+        }
+      */
+        return cellView;
+    }
+    
 }
 
